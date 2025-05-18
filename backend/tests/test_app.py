c669fc986dc84b11326f1408c35eb78341fb1ccc
Yumeng Liu <lym20041026@gmail.com>
2025-05-17 17:46:06 -0700

    Use test client, so I don't have to run localhost everytime

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,25 +1,25 @@
 import unittest
-import requests
-
+from backend.app import app
 from backend.models import DatasetFileSearchResponse, DatasetSearchResponseItem
+from fastapi.testclient import TestClient
 
-BASE_URL = "http://0.0.0.0:8000"
+test_client = TestClient(app)
 
 class AppTestCase(unittest.TestCase):
     def test_read_main(self):
-        response = requests.get(BASE_URL)
+        response = test_client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "Hello World"})
 
     def test_search_dataset(self):
-        response = requests.post(BASE_URL + "/searchDataset", json={
+        response = test_client.post("/searchDataset", json={
             "search": "titanic"
         })
         self.assertEqual(response.status_code, 200)
         assert DatasetSearchResponseItem(**response.json()[0])
 
     def test_search_dataset_files(self):
-        response = requests.get(BASE_URL + "/searchDatasetFiles/brendan45774/test-file")
+        response = test_client.get("/searchDatasetFiles/brendan45774/test-file")
         self.assertEqual(response.status_code, 200)
         assert DatasetFileSearchResponse(**response.json())
 
