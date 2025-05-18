a443bc95e1036870a157516e6f843456fafde1fe
Yumeng Liu <lym20041026@gmail.com>
2025-05-18 15:14:42 -0700

    colors on button

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -7,25 +7,29 @@ import {
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { useShowSidebarStore } from "../utils/store";
+import { useState } from "react";
 
-const toolbarIcons = [
-  {name: "bi bi-cursor"},
-  {name: "bi bi-vector-pen"},
-    {name: "bi bi-chat-left-text"},
-  {name: "bi bi-eraser"}
-]
+const toolbarButtons = [
+		{ name: "bi bi-hand-index", isSelected: false },
+		{ name: "bi bi-vector-pen", isSelected: false },
+		{ name: "bi bi-chat-left-text", isSelected: false },
+		{ name: "bi bi-eraser", isSelected: false },
+	]
 
 export default function Canvas() {
 	const showSidebar = useShowSidebarStore((state) => state.doShow);
 	const setShowSidebar = useShowSidebarStore((state) => state.setShow);
 
-  const nodes = [
-  {
-    id: '1', // required
-    position: { x: 0, y: 0 }, // required
-    data: { label: 'Hello' }, // required
-  },
-];
+  const [selectedToolIndex, setSelectedToolIndex] = useState(0)
+
+	const nodes = [
+		{
+			id: "1", // required
+			position: { x: 0, y: 0 }, // required
+			data: { label: "Hello" }, // required
+		},
+	];
+
 
 	return (
 		<div className="w-full min-h-full">
@@ -37,20 +41,26 @@ export default function Canvas() {
 				/>
 				<Controls></Controls>
 
-        {/* sidebar control button */}
+				{/* sidebar control button */}
 				<Panel>
-					<i className="card-box bi bi-list bg-white text-lg hover:bg-gray-200" onClick={() => setShowSidebar(!showSidebar)}></i>
+					<i
+						className="card-box bi bi-list bg-white text-lg hover:bg-gray-200"
+						onClick={() => setShowSidebar(!showSidebar)}
+					></i>
 				</Panel>
 
-        {/* Toolbar */}
-        <Panel position="bottom-center">
-          <div className="flex card-box bg-white gap-8 ">
-            {toolbarIcons.map((item, index) => (<i className={`${item.name} hover:bg-gray-200 rounded-md p-2`} key={index}></i>))}
-          </div>
-        </Panel>
-       
-
-
+				{/* Toolbar */}
+				<Panel position="bottom-center">
+					<div className="flex card-box bg-white gap-8 ">
+						{toolbarButtons.map((item, index) => (
+							<i
+								className={`${item.name} rounded-md p-2 ${index === selectedToolIndex ? "bg-purple-400/50" : "hover:bg-gray-200"}`}
+								key={index}
+                onClick={() => setSelectedToolIndex(index)}
+							></i>
+						))}
+					</div>
+				</Panel>
 			</ReactFlow>
 		</div>
 	);
